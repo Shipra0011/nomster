a82c7898a12eceba548921055ac8445fc408a00b
Shipra0011 <surfriderfitness23@gmail.com>
2016-12-17 22:57:53 -0500

    Add a form to the individual place page

--- db/migrate/20161217184240_create_comments.rb
+++ db/migrate/20161217184240_create_comments.rb
@@ -2,16 +2,16 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
     	t.text :message
-    	t.string :rating 
+      t.string :rating
 
-    	t.integer :user_id
-    	t.integer :place_id
-    	t.timestamps
+      t.integer :user_id
+      t.integer :place_id
+      t.timestamps
     end
 
     add_index :comments, [:user_id, :place_id]
     add_index :comments, :place_id
-  end 
-end 
+	end 
+end
 
      
